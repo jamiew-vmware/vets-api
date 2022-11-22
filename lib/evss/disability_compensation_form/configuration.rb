ae8ffb7eb4ab8302211e80b02f63b48a688dcafb
Lucian Snare <lucian.snare@adhocteam.us>
2022-11-22 15:58:10 -0500

    [API-20671]: Add a Flipper feature to use the Dockerized instance of EVSS running on DVP

--- lib/evss/disability_compensation_form/configuration.rb
+++ lib/evss/disability_compensation_form/configuration.rb
@@ -13,7 +13,18 @@ module EVSS
       # @return [String] The base path for the EVSS 526 endpoints
       #
       def base_path
-        "#{Settings.evss.url}/#{Settings.evss.alternate_service_name}/rest/form526/v2"
+        if Flipper.enabled? :form526_dvp
+          dvp_base_path()
+        else
+          "#{Settings.evss.url}/#{Settings.evss.alternate_service_name}/rest/form526/v2"
+        end
+      end
+
+      # @return [String] The base path for the EVSS Form 526 endpoints in DVP
+      #
+      def dvp_base_path
+        dvp_env = Settings.evss.dvp.current_env
+        "#{Settings.evss.dvp[dvp_env].url}/#{Settings.evss.dvp[dvp_env].service_name}/rest/form526/v2"
       end
 
       # @return [String] The name of the service, used by breakers to set a metric name for the service
