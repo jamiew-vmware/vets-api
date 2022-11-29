febb7f868bd898494b52daa9e108f4feb144a2de
oleksii-morgun-va <87077843+oleksii-morgun-va@users.noreply.github.com>
2022-11-29 15:55:26 +0000

    Pending changes exported from your codespace

--- modules/my_health/app/controllers/my_health/sm_controller.rb
+++ modules/my_health/app/controllers/my_health/sm_controller.rb
@@ -14,7 +14,8 @@ module MyHealth
     def client
       # @client ||= SM::Client.new(session: { user_id: current_user.mhv_correlation_id })
       # @client ||= SM::Client.new(session: { user_id: 9792157 }) # STAGING USER
-      @client ||= SM::Client.new(session: { user_id: 1571704 }) # DEV USER
+      # @client ||= SM::Client.new(session: { user_id: 1571704 }) # DEV USER
+      @client ||= SM::Client.new(session: { user_id: 7366505 }) # SYST MHV smautotest4
     end
 
     def authorize
