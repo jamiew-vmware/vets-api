348fd88aaa2836f1ead9a9d17e2225599f8e8bb7
Joseph Hall <joe.hall@thoughtworks.com>
2023-01-06 11:39:39 -0500

    removes dead tests and unreferenced file

--- spec/controllers/v0/virtual_agent/virtual_agent_appeal_spec.rb
+++ spec/controllers/v0/virtual_agent/virtual_agent_appeal_spec.rb
@@ -389,44 +389,5 @@ RSpec.describe 'VirtualAgentAppeals', type: :request do
       end
     end
 
-    describe 'VirtualAgentStoreUserInfoJob' do
-      context 'when virtual_agent_user_access_records toggle is on'
-      it 'runs with user info and appeals as action type' do
-        skip 'the toggle has been removed, per va-virtual-agent#758; should be removed after verifying production'
-        sign_in_as(user)
-        kms = instance_double(KmsEncrypted::Box)
-        allow(kms).to receive(:encrypt).and_return('encrypted_ssn')
-
-        allow(KmsEncrypted::Box)
-          .to receive(:new)
-          .and_return(kms)
-
-        allow(VirtualAgentStoreUserInfoJob).to receive(:perform_async)
-
-        VCR.use_cassette('caseflow/virtual_agent_appeals/recent_open_compensation_appeal') do
-          get '/v0/virtual_agent/appeal'
-          expected_user_info_object = { first_name: user.first_name, last_name: user.last_name,
-                                        ssn: 'encrypted_ssn', icn: user.icn }
-          expect(kms).to have_received(:encrypt).with(user.ssn)
-          expect(VirtualAgentStoreUserInfoJob).to have_received(:perform_async).with(expected_user_info_object,
-                                                                                     'appeals', kms, {})
-        end
-        allow(Settings).to receive(:vsp_environment).and_call_original
-      end
-    end
-
-    context 'when virtual_agent_user_access_records toggle is off' do
-      it 'does not store user data' do
-        skip 'the toggle has been removed, per va-virtual-agent#758; should be removed after verifying production'
-        sign_in_as(user)
-        allow(VirtualAgentStoreUserInfoJob).to receive(:perform_async)
-
-        VCR.use_cassette('caseflow/virtual_agent_appeals/recent_open_compensation_appeal') do
-          get '/v0/virtual_agent/appeal'
-        end
-
-        expect(VirtualAgentStoreUserInfoJob).not_to receive(:perform_async)
-      end
-    end
   end
 end
