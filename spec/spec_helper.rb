dc32a915224408f0c40d81dd552b857189ba4421
Eric Boehs <eric@boehs.com>
2023-01-11 14:26:21 -0600

    REMOVE ME: Temp commit for running LastKeywordArg cop

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,20 @@ require 'support/okta_users_helpers'
 require 'pundit/rspec'
 require 'rspec/its'
 require 'rspec/retry'
+if ENV.key?('RECORD_DEPRECATIONS')
+  require 'deprecation_toolkit'
+  require 'deprecation_toolkit/rspec'
+  DeprecationToolkit::Configuration.test_runner = :rspec
+  DeprecationToolkit::Configuration.deprecation_path = 'deprecations'
+  DeprecationToolkit::Configuration.behavior = DeprecationToolkit::Behaviors::Record
+  Warning[:deprecated] = true
+
+  kwargs_warnings = [
+    # Taken from https://github.com/jeremyevans/ruby-warning/blob/1.1.0/lib/warning.rb#L18
+    %r{warning: (?:Using the last argument (?:for `.+' )?as keyword parameters is deprecated; maybe \*\* should be added to the call|Passing the keyword argument (?:for `.+' )?as the last hash parameter is deprecated|Splitting the last argument (?:for `.+' )?into positional and keyword parameters is deprecated|The called method (?:`.+' )?is defined here)\n\z}
+  ]
+  DeprecationToolkit::Configuration.warnings_treated_as_deprecation = kwargs_warnings
+end
 
 # By default run SimpleCov, but allow an environment variable to disable.
 unless ENV['NOCOVERAGE']
