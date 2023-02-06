f68f47e3b7c4a11b3fd52075066756e7dca1f058
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2023-02-06 15:43:19 -0500

    cleanup

--- lib/common/virus_scan.rb
+++ lib/common/virus_scan.rb
@@ -5,14 +5,7 @@ module Common
     module_function
 
     def scan(file_path)
-      # `clamd` runs within service group, needs group read
       File.chmod(0o640, file_path)
-
-      # NOTE: If using custom_args, no other arguments can be passed to
-      # ClamScan::Client.scan. All other arguments will be ignored
-      # args = ['-c', Rails.root.join('config', 'clamd.conf').to_s, file_path]
-      # ClamScan::Client.scan(custom_args: args)
-
       clamav = TCPSocket.open('127.0.0.1', 3310)
       stripped_filename = file_path.gsub(%r{^tmp/}, '')
       request = "SCAN /vets-api/#{stripped_filename}" # call the shared volume on clamav container
