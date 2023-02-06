c70ee1f8d9a6e5aa292b5195f09fa5467f077f7f
Lindsey Hattamer <lindsey.hattamer@oddball.io>
2023-02-06 15:20:34 -0500

    call clamav via TCP socket

--- lib/common/virus_scan.rb
+++ lib/common/virus_scan.rb
@@ -10,8 +10,16 @@ module Common
 
       # NOTE: If using custom_args, no other arguments can be passed to
       # ClamScan::Client.scan. All other arguments will be ignored
-      args = ['-c', Rails.root.join('config', 'clamd.conf').to_s, file_path]
-      ClamScan::Client.scan(custom_args: args)
+      # args = ['-c', Rails.root.join('config', 'clamd.conf').to_s, file_path]
+      # ClamScan::Client.scan(custom_args: args)
+
+      clamav = TCPSocket.open("127.0.0.1", 3310)
+      stripped_filename = file_path.gsub(/^tmp\//, "")
+      request = "SCAN /vets-api/#{stripped_filename}" #call the shared volume on clamav container
+      clamav.puts(request)
+      response = clamav.gets
+      response
+      clamav.close
     end
   end
 end
