cdb46dd50c4570d37a9417d92cf003a0f0496023
robertbylight <roberto.rodriguez@bylight.com>
2023-02-11 12:04:10 -0500

    [MHV 39021] replaced the id value with the message_id value on the serializer

--- modules/my_health/app/serializers/my_health/v1/message_details_serializer.rb
+++ modules/my_health/app/serializers/my_health/v1/message_details_serializer.rb
@@ -3,12 +3,19 @@
 module MyHealth
   module V1
     class MessageDetailsSerializer < MessagesSerializer
+      def id
+        object.message_id
+      end
+      attribute :message_id
       attribute :thread_id
-      attribute :folder_id
+      attribute :folder_id 
       attribute :message_body
       attribute :draft_date
       attribute :to_date
       attribute :has_attachments
+
+      link(:self) { MyHealth::UrlHelper.new.v1_message_url(object.message_id) }
+
     end
   end
 end
