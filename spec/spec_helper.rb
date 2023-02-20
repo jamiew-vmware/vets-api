c7832b46031fda0ea41cf5d019b3016b1b87a400
Eric Boehs <eric@boehs.com>
2023-02-20 15:52:47 -0600

    Record all

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -22,11 +22,7 @@ if ENV.key?('RECORD_DEPRECATIONS')
   DeprecationToolkit::Configuration.behavior = DeprecationToolkit::Behaviors::Record
   Warning[:deprecated] = true
 
-  kwargs_warnings = [
-    # Taken from https://github.com/jeremyevans/ruby-warning/blob/1.1.0/lib/warning.rb#L18
-    %r{warning: (?:Using the last argument (?:for `.+' )?as keyword parameters is deprecated; maybe \*\* should be added to the call|Passing the keyword argument (?:for `.+' )?as the last hash parameter is deprecated|Splitting the last argument (?:for `.+' )?into positional and keyword parameters is deprecated|The called method (?:`.+' )?is defined here)\n\z}
-  ]
-  DeprecationToolkit::Configuration.warnings_treated_as_deprecation = kwargs_warnings
+  DeprecationToolkit::Configuration.warnings_treated_as_deprecation = [//]
 end
 
 # By default run SimpleCov, but allow an environment variable to disable.
