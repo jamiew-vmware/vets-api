5a470502ad131a5bc80d90907a6ea2bcc1d0ebe2
Neal Hill <neal@docme360.com>
2023-05-02 02:36:06 -0700

    Update to include missing form options

--- app/models/saved_claim/education_benefits/va_1995.rb
+++ app/models/saved_claim/education_benefits/va_1995.rb
@@ -6,17 +6,19 @@ class SavedClaim::EducationBenefits::VA1995 < SavedClaim::EducationBenefits
   # pulled from https://github.com/department-of-veterans-affairs/vets-website/blob/f27b8a5ffe4e2f9357d6c501c9a6a73dacdad0e1/src/applications/edu-benefits/utils/helpers.jsx#L100
   BENEFIT_TITLE_FOR_1995 = {
     'chapter30' => 'Montgomery GI Bill (MGIB or Chapter 30) Education Assistance Program',
-    'chapter33' => 'Post-9/11 GI Bill (Chapter 33)',
+    'chapter33a' => 'Post-9/11 GI Bill (Chapter 33)',
+    'chapter33b' => 'The Fry Scholarship (Chapter 33)',
     'chapter1606' => 'Montgomery GI Bill Selected Reserve (MGIB-SR or Chapter 1606) Educational Assistance Program',
-    'chapter32' => 'Post-Vietnam Era Veterans’ Educational Assistance Program (VEAP or chapter 32)'
+    'chapter32' => 'Post-Vietnam Era Veterans’ Educational Assistance Program (VEAP or chapter 32)',
+    'transferOfEntitlement' => 'Transfer of Entitlement Program'
   }.freeze
 
   def after_submit(_user)
+    return unless Flipper.enabled?(:form1995_confirmation_email)
+
     parsed_form_data ||= JSON.parse(form)
     email = parsed_form_data['email']
     return if email.blank?
-
-    return unless Flipper.enabled?(:form1995_confirmation_email)
 
     send_confirmation_email(parsed_form_data, email)
   end
@@ -29,8 +31,7 @@ class SavedClaim::EducationBenefits::VA1995 < SavedClaim::EducationBenefits
       Settings.vanotify.services.va_gov.template_id.form1995_confirmation_email,
       {
         'first_name' => parsed_form.dig('veteranFullName', 'first')&.upcase.presence,
-        'benefits' => benefits_claimed(parsed_form_data),
-        'benefit_relinquished' => '',
+        'benefit' => benefit_claimed(parsed_form_data),
         'date_submitted' => Time.zone.today.strftime('%B %d, %Y'),
         'confirmation_number' => education_benefits_claim.confirmation_number,
         'regional_office_address' => regional_office_address
@@ -38,10 +39,8 @@ class SavedClaim::EducationBenefits::VA1995 < SavedClaim::EducationBenefits
     )
   end
 
-  def benefits_claimed(parsed_form_data)
-    %w[chapter30 chapter33 chapter1606 chapter32]
-      .map { |benefit| parsed_form_data[benefit] ? BENEFIT_TITLE_FOR_1995[benefit] : nil }
-      .compact
-      .join("\n\n^")
+  def benefit_claimed(parsed_form_data)
+    benefit ||= parsed_form_data['benefit']
+    BENEFIT_TITLE_FOR_1995[benefit]
   end
 end
