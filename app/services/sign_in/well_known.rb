77c1478c47e6f9e14075e86bd66242a7e254f712
Ty Barnes <ty_barnes_jobs@yahoo.com>
2023-05-05 13:30:43 -0400

    changes class name and file structure

--- app/services/sign_in/well_known.rb
+++ app/services/sign_in/well_known.rb
@@ -1,42 +1,40 @@
 # frozen_string_literal: true
 
 module SignIn
-  module WellKnown
-    class Generator
-      def perform
-        well_known_body
-      end
+  class WellKnownGenerator
+    def perform
+      well_known_body
+    end
 
-      private
+    private
 
-      def well_known_body
-        end_points.merge(types).merge(methods)
-      end
+    def well_known_body
+      end_points.merge(types).merge(methods)
+    end
 
-      def end_points
-        {
-          issuer: Settings.hostname,
-          authorization_endpoint: "#{Settings.hostname}#{Constants::Auth::AUTHORIZATION_ROUTE_PATH}",
-          token_endpoint: "#{Settings.hostname}#{Constants::Auth::TOKEN_ROUTE_PATH}",
-          token_refresh_endpoint: "#{Settings.hostname}#{Constants::Auth::REFRESH_ROUTE_PATH}",
-          introspection_endpoint: "#{Settings.hostname}#{Constants::Auth::INTROSPECTION_ROUTE_PATH}",
-          end_session_endpoint: "#{Settings.hostname}#{Constants::Auth::END_SESSION_ROUTE_PATH}",
-          token_revocation_individual_endpoint: "#{Settings.hostname}#{Constants::Auth::TOKEN_REVOCATION_INDIVIDUAL_ROUTE_PATH}", # rubocop:disable Layout/LineLength
-          revocation_endpoint: "#{Settings.hostname}#{Constants::Auth::REVOCATION_ROUTE_PATH}"
-        }
-      end
+    def end_points
+      {
+        issuer: Settings.hostname,
+        authorization_endpoint: "#{Settings.hostname}#{Constants::Auth::AUTHORIZATION_ROUTE_PATH}",
+        token_endpoint: "#{Settings.hostname}#{Constants::Auth::TOKEN_ROUTE_PATH}",
+        token_refresh_endpoint: "#{Settings.hostname}#{Constants::Auth::REFRESH_ROUTE_PATH}",
+        introspection_endpoint: "#{Settings.hostname}#{Constants::Auth::INTROSPECTION_ROUTE_PATH}",
+        end_session_endpoint: "#{Settings.hostname}#{Constants::Auth::END_SESSION_ROUTE_PATH}",
+        token_revocation_individual_endpoint: "#{Settings.hostname}#{Constants::Auth::TOKEN_REVOCATION_INDIVIDUAL_ROUTE_PATH}", # rubocop:disable Layout/LineLength
+        revocation_endpoint: "#{Settings.hostname}#{Constants::Auth::REVOCATION_ROUTE_PATH}"
+      }
+    end
 
-      def methods
-        {
-          code_challenge_methods_supported: Constants::Auth::CODE_CHALLENGE_METHOD
-        }
-      end
+    def methods
+      {
+        code_challenge_methods_supported: Constants::Auth::CODE_CHALLENGE_METHOD
+      }
+    end
 
-      def types
-        {
-          grant_types_supported: Constants::Auth::GRANT_TYPE
-        }
-      end
+    def types
+      {
+        grant_types_supported: Constants::Auth::GRANT_TYPE
+      }
     end
   end
 end
