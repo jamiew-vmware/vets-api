92adf7d079a875d219f9e7684495284fe237557a
Eddie Glenn <eddieglenn@navapbc.com>
2023-05-11 13:53:39 -0500

    fix rubocop errors, add tests to lighthouse itf provider

--- spec/requests/swagger_spec.rb
+++ spec/requests/swagger_spec.rb
@@ -1118,18 +1118,6 @@ RSpec.describe 'the API documentation', type: %i[apivore request], order: :defin
         end
       end
 
-      it 'supports getting an active compensation intent to file' do
-        expect(subject).to validate(:get, '/v0/intent_to_file/{type}/active', 401, 'type' => 'compensation')
-        VCR.use_cassette('evss/intent_to_file/active_compensation') do
-          expect(subject).to validate(
-            :get,
-            '/v0/intent_to_file/{type}/active',
-            200,
-            headers.update('type' => 'compensation')
-          )
-        end
-      end
-
       it 'supports creating an active compensation intent to file' do
         expect(subject).to validate(:post, '/v0/intent_to_file/{type}', 401, 'type' => 'compensation')
         VCR.use_cassette('evss/intent_to_file/create_compensation') do
