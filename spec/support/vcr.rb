c0a7b592653b05c44cbc1351fdbddb9062ef9e18
Austin Covrig <austin.covrig@oddball.io>
2023-05-15 13:50:35 -0400

    Add RDoc to intake_sites and filter brd key

--- spec/support/vcr.rb
+++ spec/support/vcr.rb
@@ -15,6 +15,7 @@ VCR.configure do |c|
   c.filter_sensitive_data('<GIDS_URL>') { Settings.gids.url }
   c.filter_sensitive_data('<LIGHTHOUSE_API_KEY>') { Settings.decision_review.api_key }
   c.filter_sensitive_data('<LIGHTHOUSE_API_KEY>') { Settings.lighthouse.facilities.api_key }
+  c.filter_sensitive_data('<LIGHTHOUSE_BRD_API_KEY>') { Settings.brd.api_key }
   c.filter_sensitive_data('<MDOT_KEY>') { Settings.mdot.api_key }
   c.filter_sensitive_data('<MHV_HOST>') { Settings.mhv.rx.host }
   c.filter_sensitive_data('<MHV_MR_HOST>') { Settings.mhv.medical_records.host }
