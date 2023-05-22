bee0c3a28650ba435541fdcd135df5c6c3d930a4
mchae-nava <129200156+mchae-nava@users.noreply.github.com>
2023-05-21 23:42:18 -0400

    Update evss itf spec

--- spec/lib/disability_compensation/providers/intent_to_file/evss_intent_to_file_provider_spec.rb
+++ spec/lib/disability_compensation/providers/intent_to_file/evss_intent_to_file_provider_spec.rb
@@ -33,12 +33,16 @@ RSpec.describe EvssIntentToFileProvider do
     end
   end
 
-  # TODO-BDEX: Test this after fixing docker
+  # bundle exec rspec spec/lib/disability_compensation/providers/intent_to_file/evss_intent_to_file_provider_spec.rb
   it 'creates intent to file from the EVSS API' do
-    VCR.use_cassette('evss/intent_to_file/intent_to_file') do
+    VCR.use_cassette('evss/intent_to_file/create_compensation') do
       provider = EvssIntentToFileProvider.new(nil, auth_headers)
       response = provider.create_intent_to_file('compensation', '', '')
-      expect(response['intent_to_file'].length).to eq(5)
+      expect(response).to be_an_instance_of(EVSS::IntentToFile::IntentToFileResponse)
+      expect(response['intent_to_file']['type']).to eq('compensation')
+
+      # No ActiveRecord inheritance; verify the id as a workaround
+      expect(response['intent_to_file']['id']).to be_present
     end
   end
 
