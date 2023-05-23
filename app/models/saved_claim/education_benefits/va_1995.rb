053a092049d1fa889a5bb2b359a19b79e080b154
Neal Hill <neal@docme360.com>
2023-05-23 09:34:25 -0700

    Update to correct chapter33 keys

--- app/models/saved_claim/education_benefits/va_1995.rb
+++ app/models/saved_claim/education_benefits/va_1995.rb
@@ -6,8 +6,8 @@ class SavedClaim::EducationBenefits::VA1995 < SavedClaim::EducationBenefits
   # pulled from https://github.com/department-of-veterans-affairs/vets-website/blob/f27b8a5ffe4e2f9357d6c501c9a6a73dacdad0e1/src/applications/edu-benefits/utils/helpers.jsx#L100
   BENEFIT_TITLE_FOR_1995 = {
     'chapter30' => 'Montgomery GI Bill (MGIB or Chapter 30) Education Assistance Program',
-    'chapter33a' => 'Post-9/11 GI Bill (Chapter 33)',
-    'chapter33b' => 'The Fry Scholarship (Chapter 33)',
+    'chapter33Post911' => 'Post-9/11 GI Bill (Chapter 33)',
+    'chapter33FryScholarship' => 'The Fry Scholarship (Chapter 33)',
     'chapter1606' => 'Montgomery GI Bill Selected Reserve (MGIB-SR or Chapter 1606) Educational Assistance Program',
     'chapter32' => 'Post-Vietnam Era Veterans’ Educational Assistance Program (VEAP or chapter 32)',
     'transferOfEntitlement' => 'Transfer of Entitlement Program'
